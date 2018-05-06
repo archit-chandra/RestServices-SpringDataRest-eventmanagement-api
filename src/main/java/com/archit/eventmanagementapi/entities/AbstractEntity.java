a7005d95349caa07b2b4fe326942dd2a714c9d77
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-06 20:50:02 +0200

    Entity : AbstractEntity (parent entity to hold common attributes) added

---
+++ src/main/java/com/archit/eventmanagementapi/entities/AbstractEntity.java
@@ -0,0 +1,35 @@
+package com.archit.eventmanagementapi.entities;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import javax.persistence.*;
+import java.time.Instant;
+
+@MappedSuperclass
+public class AbstractEntity {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    protected long id;
+
+    @CreationTimestamp
+    @Column(updatable = false)
+    protected Instant created;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Instant getCreated() {
+        return created;
+    }
+
+    public void setCreated(Instant created) {
+        this.created = created;
+    }
+}
