410b9f431bbb38dcba6ed47906ceb8532605bd1e
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-10 22:44:46 +0200

    Configuration : for using java.time.*

--- src/main/java/com/archit/eventmanagementapi/EventmanagementApiApplication.java
+++ src/main/java/com/archit/eventmanagementapi/EventmanagementApiApplication.java
@@ -2,8 +2,15 @@ package com.archit.eventmanagementapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.convert.Jsr310Converters;
 
 @SpringBootApplication
+@EntityScan(
+        basePackageClasses = {
+                EventmanagementApiApplication.class,
+                Jsr310Converters.class // for configuring java.time.*
+        })
 public class EventmanagementApiApplication {
 
     public static void main(String[] args) {
