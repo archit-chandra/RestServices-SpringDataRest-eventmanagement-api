fdb5ab4396d435fed27975a177d2fed791056a24
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-12 16:06:32 +0200

    add findBy method in Event

--- src/main/java/com/archit/eventmanagementapi/repos/EventRepository.java
+++ src/main/java/com/archit/eventmanagementapi/repos/EventRepository.java
@@ -2,6 +2,11 @@ package com.archit.eventmanagementapi.repos;
 
 import com.archit.eventmanagementapi.entities.Event;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
+
+    List<Event> findByName(@Param("name") String name);
 }
