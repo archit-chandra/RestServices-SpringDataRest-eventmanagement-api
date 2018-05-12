a6eaf438985bf8fbf02cb3e9c2b5aa9efc267dee
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-12 18:38:38 +0200

    Controller : CheckInController (to check in an event by participant) - implemented the checks in checkIn method

--- src/main/java/com/archit/eventmanagement/controllers/CheckInController.java
+++ src/main/java/com/archit/eventmanagement/controllers/CheckInController.java
@@ -1,7 +1,11 @@
 package com.archit.eventmanagement.controllers;
 
+import com.archit.eventmanagement.controllers.exceptions.AlreadyCheckedInException;
 import com.archit.eventmanagement.entities.Participant;
+import com.archit.eventmanagement.repos.ParticipantRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -10,9 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RepositoryRestController
 @RequestMapping("/events")
 public class CheckInController {
+    @Autowired
+    private ParticipantRepository participantRepository;
 
     @PostMapping("/checkin/{id}")
     public ResponseEntity<Participant> checkIn(@PathVariable Long id) {
+        Participant participant = participantRepository.findById(id).orElse(null);
+        if (participant != null) {
+            if (participant.isCheckedIn()) {
+                throw new AlreadyCheckedInException();
+            }
+            participant.setCheckedIn(true);
+            participantRepository.save(participant);
+        } else {
+            throw new ResourceNotFoundException();
+        }
         return null;
     }
 }
