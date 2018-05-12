b17b3d5a9c018f9b2b6cf87410170b881e3cd12f
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-13 00:07:05 +0200

    configuring authentication & authorization

--- src/main/java/com/archit/eventmanagement/SecurityConfiguration.java
+++ src/main/java/com/archit/eventmanagement/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.archit.eventmanagement;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,17 +9,23 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    // defines the users and roles
+    // defines the users and roles => authentication
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        // has many other authentication types
         auth.inMemoryAuthentication()
                 .withUser("bharath").password("bharath").roles("USER")
                 .and()
                 .withUser("admin").password("admin").roles("ADMIN");
     }
 
+    // configures authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        super.configure(http);
+        http.httpBasic().and().authorizeRequests()
+                // 3 types of antMatchers methods
+                .antMatchers(HttpMethod.POST, "/events").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PUT, "/events/**").hasRole("ADMIN")
+                .antMatchers(HttpMethod.PATCH, "/events/**").hasRole("ADMIN");
     }
 }
