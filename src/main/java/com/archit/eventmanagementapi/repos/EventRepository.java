f5dc58865fd43185d64726ac34b0dca1d9eb28d3
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-12 17:42:23 +0200

    find by multiple parameters (searching by name and zoneId)

--- src/main/java/com/archit/eventmanagementapi/repos/EventRepository.java
+++ src/main/java/com/archit/eventmanagementapi/repos/EventRepository.java
@@ -6,11 +6,20 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+import java.time.ZoneId;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
+
+    /**
+     * findOne(), findAll() are already provided for the given Entity
+     */
 
     //List<Event> findByName(@Param("name") String name);
 
     Page<Event> findByName(@Param("name") String name, Pageable pageable);
+
+    Page<Event> findByNameAndZoneId(
+            @Param("name") String name,
+            @Param("zoneId") ZoneId zoneId,
+            Pageable pageable);
 }
