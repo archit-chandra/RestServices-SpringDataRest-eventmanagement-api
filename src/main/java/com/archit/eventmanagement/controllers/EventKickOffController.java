1aaf68914b124f1f5c06f7ba3e44e3bf4b353b41
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-12 18:24:06 +0200

    Controller : EventKickOffController (to start an event) - added the start method

--- src/main/java/com/archit/eventmanagement/controllers/EventKickOffController.java
+++ src/main/java/com/archit/eventmanagement/controllers/EventKickOffController.java
@@ -1,6 +1,13 @@
 package com.archit.eventmanagement.controllers;
 
+import com.archit.eventmanagement.entities.Event;
+import com.archit.eventmanagement.repos.EventRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // tells Spring that this is a Controller
@@ -10,4 +17,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RepositoryRestController
 @RequestMapping("/events")
 public class EventKickOffController {
+
+    @Autowired
+    private EventRepository eventRepository;
+
+    @PostMapping("/start/{id}")
+    public ResponseEntity start(@PathVariable Long id) {
+        Event event = eventRepository.findById(id).orElse(null);
+        if (event == null) {
+            throw new ResourceNotFoundException();
+        }
+        event.setStarted(true);
+        eventRepository.save(event);
+        return ResponseEntity.ok(event.getName() + " has started");
+    }
 }
