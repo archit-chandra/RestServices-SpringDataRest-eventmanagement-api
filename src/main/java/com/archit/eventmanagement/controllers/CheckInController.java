2e5af33dcaaff22a2c718959ea07d5a02cb0b153
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-12 18:48:37 +0200

    Controller : CheckInController (to check in an event by participant) - returning a HAL Response using the entity

--- src/main/java/com/archit/eventmanagement/controllers/CheckInController.java
+++ src/main/java/com/archit/eventmanagement/controllers/CheckInController.java
@@ -4,6 +4,8 @@ import com.archit.eventmanagement.controllers.exceptions.AlreadyCheckedInExcepti
 import com.archit.eventmanagement.entities.Participant;
 import com.archit.eventmanagement.repos.ParticipantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.PersistentEntityResource;
+import org.springframework.data.rest.webmvc.PersistentEntityResourceAssembler;
 import org.springframework.data.rest.webmvc.RepositoryRestController;
 import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.http.ResponseEntity;
@@ -18,7 +20,8 @@ public class CheckInController {
     private ParticipantRepository participantRepository;
 
     @PostMapping("/checkin/{id}")
-    public ResponseEntity<Participant> checkIn(@PathVariable Long id) {
+    public ResponseEntity<PersistentEntityResource> checkIn(@PathVariable Long id,
+                                                            PersistentEntityResourceAssembler assembler) {
         Participant participant = participantRepository.findById(id).orElse(null);
         if (participant != null) {
             if (participant.isCheckedIn()) {
@@ -29,6 +32,6 @@ public class CheckInController {
         } else {
             throw new ResourceNotFoundException();
         }
-        return null;
+        return ResponseEntity.ok(assembler.toResource(participant));
     }
 }
