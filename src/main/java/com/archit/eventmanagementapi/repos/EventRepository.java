0058e27b00eaa5a6f094e22f018e8f7fbdd99faa
DESKTOP-BL7US23\architbiet <architbiet@gmail.com>
2018-05-12 16:11:47 +0200

    adding paging support to finders

--- src/main/java/com/archit/eventmanagementapi/repos/EventRepository.java
+++ src/main/java/com/archit/eventmanagementapi/repos/EventRepository.java
@@ -1,6 +1,8 @@
 package com.archit.eventmanagementapi.repos;
 
 import com.archit.eventmanagementapi.entities.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 
@@ -8,5 +10,7 @@ import java.util.List;
 
 public interface EventRepository extends PagingAndSortingRepository<Event, Long> {
 
-    List<Event> findByName(@Param("name") String name);
+    //List<Event> findByName(@Param("name") String name);
+
+    Page<Event> findByName(@Param("name") String name, Pageable pageable);
 }
