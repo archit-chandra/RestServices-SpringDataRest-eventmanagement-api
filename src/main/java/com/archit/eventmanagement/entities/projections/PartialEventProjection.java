dab4988c5bbe7c4ba2118395c19baf81dc7ad4d6
archit_chandra <send2archit.chandra@gmail.com>
2019-06-18 01:39:40 +0200

    added notes: @Projection

--- src/main/java/com/archit/eventmanagement/entities/projections/PartialEventProjection.java
+++ src/main/java/com/archit/eventmanagement/entities/projections/PartialEventProjection.java
@@ -6,6 +6,10 @@ import org.springframework.data.rest.core.config.Projection;
 import java.time.Instant;
 import java.time.ZonedDateTime;
 
+// NOTES: @Projection
+//  1. To create custom views of the models
+//  2. Similarly, excerpts are projections which we apply as default views to resource collections.
+//  3. https://www.baeldung.com/spring-data-rest-projections-excerpts
 @Projection(name = "partial", types = {Event.class})
 public interface PartialEventProjection {
 
