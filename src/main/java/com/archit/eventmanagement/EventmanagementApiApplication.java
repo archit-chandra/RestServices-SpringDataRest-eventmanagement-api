d604a696f777adbe087bdd5d6b9de2e489ca1bac
archit_chandra <send2archit.chandra@gmail.com>
2019-06-18 01:40:20 +0200

    added notes: @EntityScan, basePackageClasses, Jsr310Converters

--- src/main/java/com/archit/eventmanagement/EventmanagementApiApplication.java
+++ src/main/java/com/archit/eventmanagement/EventmanagementApiApplication.java
@@ -6,9 +6,18 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.convert.Jsr310Converters;
 
 @SpringBootApplication
+// NOTES: @EntityScan
+//  1. It does not create beans,
+//     only identifies which classes should be used by a specific persistence context.
+//  2. Here, basePackageClasses refers the base packages which contains the listed classes
+//  3. @ComponentScan annotation is used to automatically create beans for every class annotated with
+//     @Component, @Service, @Controller, @RestController, @Repository, ...
+//     and adds them to the Spring container (allowing them to be @Autowired).
 @EntityScan(
         basePackageClasses = {
                 EventmanagementApiApplication.class,
+                // NOTES: Jsr310Converters
+                //  1. for configuring java.time.*
                 Jsr310Converters.class // for configuring java.time.*
         })
 public class EventmanagementApiApplication {
